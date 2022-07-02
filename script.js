1942cb3372bbc8187c9592c5dccca3db8af87ded
Knot <akthakorn@gmail.com>
2022-07-02 21:57:31 +0700

    update

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function resizeEnd() {
 // Change Language
 
 var th = {
-    textheader: "ไทม์ไลน์การเริ่มฝึกงานของผมเมื่อทำงานจากที่บ้าน",
+    textheader: "สวัสดีครับ ผมชื่อนายเอกฐากร ไตรมิตร (น๊อต)",
     text7: "ตื่นจากเตียงและอาบน้ำ",
     text72: "แต่งกายด้วยชุดสุภาพเรียบร้อย",
     text725: "เตรียมน้ำร้อนและชงกาแฟ",
@@ -118,10 +118,16 @@ var th = {
     text8: "เตรียมการประชุมประจำวัน",
     text9: "เริ่มการประชุม",
     namet: "ลายเช็นผู้ปกครอง",
+    txtm: "ไทม์ไลน์การเริ่มฝึกงานของผม เมื่อต้องทำงานจากที่บ้าน",
+    cme: "สิ่งที่ผมต้องปรับปรุงช่วงเริ่มฝึกงาน",
+    c1: "เริ่มปรับเวลาการนอนพักผ่อนร่างกายให้เร็วขึ้น เพื่อการพักผ่อนที่เพียงพอสำหรับการตื่นเช้าเริ่มฝึกงานวันต่อไป",
+    c2: "ปรับความเข้าใจกาลเทศะของการทำงาน หรือการดำรงชีวิตร่วมกับผู้อื่น เพื่อไม่ให้เกิดความขัดแย้งและข้อผิดพลาด",
+    c3: "ช่วงเวลาเริ่มงานต้องเจาะจงกับสิ่งที่ได้รับมอบหมายเป็นอย่างแรกเสมอ เพื่อไม่ให้เป็นภาระต่อผู้ร่วมงาน",
+    c4: "พัฒนาทักษะด้านการสื่อสารและการสนทนา เพื่อการพูดคุยอธิบายสิ่งที่ต้องการจะสื่อความหมายให้เข้าใจง่ายยิ่งขึ้น",
     btn_language: "ไทย"
   };
   var en = {
-    textheader: "My internship start timeline when work from home.",
+    textheader: "Hi, my name is Mr.Akthakorn Traimtir (Knot)",
     text7: "Wake up from bed and take a shower.",
     text72: "Get dressed in neat clothes.",
     text725: "Prepare hot water and make coffee.",
@@ -129,6 +135,12 @@ var th = {
     text8: "Prepare daily meeting.",
     text9: "Start the meeting.",
     namet: "Parent's signature",
+    txtm: "My start internship timeline when work from home.",
+    cme: "Things to improve when starting an internship for me",
+    c1: "Start adjusting the time to sleep and rest your body faster. To have enough rest for waking up early for the next day's training.",
+    c2: "Improve circumstance understanding of the working times or living with others to avoid conflicts and errors",
+    c3: "The start time must always be specific to the assignment first. in order not to be a burden on the co-workers",
+    c4: "Improve communication and conversation skills in order to discuss and explain what needs to be conveyed in a way that is easier to understand",
     btn_language: "EN"
   };
   
@@ -144,7 +156,13 @@ var th = {
       $("#text8").text(settext("text8"));
       $("#text9").text(settext("text9"));
       $("#namet").text(settext("namet"));
+      $("#txtm").text(settext("txtm"));
       $("#btn_language").text(settext("btn_language"));
+      $("#cme").text(settext("cme"));
+      $("#c1").text(settext("c1"));
+      $("#c2").text(settext("c2"));
+      $("#c3").text(settext("c3"));
+      $("#c4").text(settext("c4"));
     }
   }
   
